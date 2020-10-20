a2ecf1ff391187f93fe30816fcf23b3eb2142d96
kinder <ferraz.alkindar@gmail.com>
2020-10-20 16:13:02 -0300

    Reestruturar aquivo js em 3:
    um arquivo responsavel pelo painel de disciplinas,
    outro pela barra de tarefas
    outro pelo tema noturno

---
+++ src/js/barraTarefas.js
@@ -0,0 +1,112 @@
+/* Aqui está o código javascritp que monta a barra lateral.
+ * A lógica é semelhante à usada para o painel de disciplinas,
+ * com alguns filtros um pouco mais elaborados. */
+
+/* Este objeto garante a integridade dos valores de filtro, isto é,
+ * em nenhum momento o valor de filtroTarefas pode ser outro objeto. */
+const filtros = {
+  TUDO: "tudo",
+  ATRASADO: "atrasado",
+  NO_PRAZO: "prazo"
+};
+let filtroTarefas = filtros.TUDO;
+
+/* Este objeto é usado para formatar as datas inseridas no badge */
+const options = {
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric'
+};
+
+/* O objeto no DOM onde os bagdes serão inseridos */
+const sidebarTarefas = document.querySelector("#tarefas");
+
+/* Os dados para montar o badge */
+const entregas = [{
+  nome: "IPv4",
+  disc: 1,
+  prazo: new Date(2020, 9, 14, 00, 00),
+  envio: false
+}, {
+  nome: "CSS -- Grid",
+  disc: 1,
+  prazo: new Date(2020, 10, 14, 00, 00),
+  envio: true
+}, {
+  nome: "Functions e Triggers",
+  disc: 1,
+  prazo: new Date(2020, 10, 14, 13, 00),
+  envio: false
+}, {
+  nome: "Avaliação",
+  disc: 1,
+  prazo: new Date(2020, 10, 14, 23, 59),
+  envio: false
+}];
+
+/* Esta função é passada como filtro sobre o vetor de tarefas:
+ * Seleciona as tarefas de acordo com o critério escolhido pelo usuário:
+ *   todas: nao filtra;
+ *   atrasado: retorna aquelas com prazo anterior a data atual
+               E nao enviadas;
+ *   no prazo: retorna as tarefas que ainda podem ser entregadas ou alteradas.
+ */
+const tarefaFiltro = (t) => {
+  if (filtroTarefas == filtros.ATRASADO)
+    return t.prazo.getTime() < Date.now() && !t.envio;
+
+  else if (filtroTarefas == filtros.NO_PRAZO)
+    return t.prazo.getTime() >= Date.now();
+
+  else
+    return true;
+};
+
+/* Monta um badge, com o icone e a classe apropriada,
+ * coloca-lo como elemento filho do elemento DOM #tarefas. */
+const displayBadgesTarefas = (t) => {
+  const badge = document.createElement("div");
+  const title = document.createElement("h4");
+  const icon = document.createElement("img");
+  const labels = document.createElement("div");
+  const data = document.createElement("p");
+  const disc = document.createElement("p");
+
+  let date = t.prazo.toLocaleDateString("pt-BR", options);
+  badge.setAttribute("class", "badge");
+
+  title.innerText = t.nome;
+  data.innerText = date;
+
+  if (t.envio) {
+    badge.classList.add("enviado");
+    icon.setAttribute("src", "../media/alarm-check.svg");
+  } else if (t.prazo.getTime() < Date.now()) {
+    badge.classList.add("atrasado");
+    icon.setAttribute("src", "../media/alarm-off.svg");
+  } else
+    icon.setAttribute("src", "../media/alarm-add.svg");
+
+
+  icon.setAttribute("class", "icon");
+
+  labels.appendChild(title);
+  labels.appendChild(data);
+
+  badge.appendChild(icon);
+  badge.appendChild(labels);
+
+  sidebarTarefas.appendChild(badge);
+};
+
+/* Limpa a barra lateral e mapeia a função de mostrar os badges sobre
+ * o vetor de dados. */
+const displayBarraTarefas = () => {
+  sidebarTarefas.innerHTML = null;
+  entregas.filter(tarefaFiltro).map(displayBadgesTarefas);
+};
+
+displayBarraTarefas();
+
+/*Envent listeners */
---
+++ src/js/painelDisciplinas.js
@@ -0,0 +1,113 @@
+/* O código relevante ao painel de disciplinas.
+ * As informações relevantes a um semestre estão
+ * guardadas no objeto disc, abaixo.
+ * A função displayPainel seleciona o semestre relavante,
+ * e mapeia sobre o vetor. Para cada objeto, coloca um card
+ * referente à disciplina no elemento #painel do documento.
+ *
+ * Em uma aplicação real, com acesso a uma API e back-end, estes dados
+ * seriam mais completos e carregados de acordo com o usuário logado.
+ * Como demostração de conceito, consideramos os dados dummy a seguir
+ * apropriados.
+*/
+
+/* Consts e variaveis: */
+const disc = {
+  "1 semestre": [{
+    codigo: 1,
+    nome: "Desenvolvimento WEB 1",
+    sigla: "DW1A3",
+    prof: "Josceli",
+    aulasSemestre: 19,
+    concluidos: 18
+  },{
+    codigo: 2,
+    nome: "Estatistica e probabilidade",
+    sigla: "ESPA3",
+    prof: "Maria Eduarda",
+    aulasSemestre: 19,
+    concluidos: 18
+  },{
+    codigo: 3,
+    nome: "Desenvolvimento WEB 1",
+    sigla: "DW1A3",
+    prof: "Josceli",
+    aulasSemestre: 19,
+    concluidos: 18
+  },{
+    codigo: 4,
+    nome: "Estatistica e probabilidade",
+    sigla: "ESPA3",
+    prof: "Maria Eduarda",
+    aulasSemestre: 19,
+    concluidos: 18
+  },{
+    codigo: 5,
+    nome: "Desenvolvimento WEB 1",
+    sigla: "DW1A3",
+    prof: "Josceli",
+    aulasSemestre: 19,
+    concluidos: 18
+  },{
+    codigo: 6,
+    nome: "Estatistica e probabilidade",
+    sigla: "ESPA3",
+    prof: "Maria Eduarda",
+    aulasSemestre: 19,
+    concluidos: 18
+  }],
+  "2 semestre": []
+};
+const painel = document.querySelector("#painel");
+let semestre = "1 semestre";
+
+/* Funções */
+/* Troca o semestre e remonta o painel */
+const trocarSemestre = (s) => {
+  if (disc[s])
+    semestre = s;
+};
+
+/* Constrói o card */
+const displayDiscCards = (d) => {
+  let card = document.createElement("div");
+  let label = document.createElement("div");
+  let icon = document.createElement("img");
+  let titulo = document.createElement("h4");
+  let sigla = document.createElement("h3");
+  let prof = document.createElement("h4");
+  let aulas = document.createElement("p");
+
+  titulo.innerText = d.nome;
+  sigla.innerText = d.sigla;
+  prof.innerText = d.prof;
+
+  aulas.innerHTML = "<span>Aulas totais:";
+  aulas.innerHTML += "<span>" + d.aulasSemestre;
+  aulas.innerHTML += "<span>Aulas restantes:";
+  aulas.innerHTML += "<span>" + (d.aulasSemestre - d.concluidos);
+
+  icon.setAttribute("src", "../media/school-hat.svg");
+  label.appendChild(icon);
+  label.appendChild(sigla);
+
+  card.appendChild(label);
+  card.appendChild(titulo);
+  card.appendChild(prof);
+  card.appendChild(aulas);
+
+  card.classList.add("disc-card");
+  icon.classList.add("icon");
+
+  painel.appendChild(card);
+};
+
+/* Mapeia o vetor apropriado com a funcão de construir cards */
+const displayPainel = () => {
+  painel.innerHTML = null;
+  disc[semestre].map(displayDiscCards);
+};
+
+/* Chamada inicial da funcão anterior, para que a página se
+ * abra completa */
+displayPainel();
