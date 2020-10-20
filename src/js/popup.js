d2bf3bf64e02a372634e7fef84087b9fb9db3cae
kinder <ferraz.alkindar@gmail.com>
2020-10-20 18:19:03 -0300

    Refatorar popups:
    adicionar js para fazer com que eles funcionem.
    renomear teste.js para popup.js
    
    (7 linhas pra mostrar qualquer popup?????!!!! eu <3 js)

---
+++ src/js/popup.js
@@ -0,0 +1,37 @@
+/* Tema noturno e popups.
+ * Neste arquivo serãp definidas as funções necessarias para aplicar o
+ * tema escuro e os event listeners para mostrar os popups */
+
+let tema = "dia";
+
+// montar o elemento link com o css
+let stylesheetNoturno = document.createElement("link");
+stylesheetNoturno.rel = "stylesheet";
+stylesheetNoturno.type = "text/css";
+stylesheetNoturno.href = "../css/noite.css";
+
+/* Quando solicitado, aplicar o css */
+const switchTema = () => {
+  const header = document.querySelector("head");
+  if (tema == "dia"){
+    header.appendChild(stylesheetNoturno);
+    tema = "noite";
+  } else {
+    header.removeChild(stylesheetNoturno);
+    tema = "dia";
+  }
+};
+
+const temaSwitch = document.querySelector("#tema-switch");
+temaSwitch.addEventListener("click", switchTema);
+
+/* PopUps */
+const popups = document.querySelectorAll(".popup");
+
+const togglePopup = (e) => {
+  e.preventDefault();
+  const popup = e.currentTarget.querySelector(".popuptext");
+  popup.classList.toggle("show");
+};
+
+popups.forEach((popup) => popup.addEventListener("click", togglePopup, true));
